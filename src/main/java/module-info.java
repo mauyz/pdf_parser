8aebc1012f25490bfe589c5fd21b4f58edc68f04
mauyz <tsiorymauyz@gmail.com>
2025-01-23 22:31:38 +0300

    config reflection

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,13 @@
 module mg.eightgroup.docparser {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.apache.pdfbox;
+    requires org.apache.pdfbox.io;
+    requires com.google.gson;
 
 
     opens mg.eightgroup.docparser to javafx.fxml;
     exports mg.eightgroup.docparser;
+
+    opens mg.eightgroup.docparser.model to com.google.gson;
 }
